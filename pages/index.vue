8655db18b25bea5c9457bfc27f4d094bedd79df4
Benjita <benjaminioticos@gmail.com>
2023-01-08 14:56:52 -0300

    FINISHED

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Youtube Tag Generator</h1>
+    <br />
+    <small>Ask the Ai anything</small>
+    <br />
+    <textarea
+      v-model="text"
+      name=""
+      id=""
+      cols="30"
+      rows="10"
+      placeholder="give me tags for a funny youtube video"
+    ></textarea>
+    <br />
+    <button @click="gpt3">Generate</button>
+    <br>
+    <h2>Results:</h2>
+    <h5>{{ results }}</h5>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+      results: "",
+    };
+  },
+  mounted() {},
+  methods: {
+    gpt3() {
+      fetch("/api/gpt3", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          textGPT: this.text,
+        }),
+      })
+        .then((response) => response.json())
+        .then((textBACK) => {
+          console.log(textBACK)
+          this.results = textBACK.result;
+        });
+    },
+  },
+};
+</script>
---
+++ server/api/gpt3.post.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: '***YOUR API KEY***',
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (event) {
+    const { textGPT } = await readBody(event);
+
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(textGPT),
+      max_tokens: 3000,
+      temperature: 0.75,
+    });
+
+    return {
+        result: completion.data.choices[0].text,
+      }
+}
+
+function generatePrompt(textGPT) {
+    return `Please provide a list of keywords that you would like to use as tags for your YouTube video separated by coma.
+    me: ${textGPT}
+    bot: `;
+  }
